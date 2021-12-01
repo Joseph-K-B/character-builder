be2cbcb93afc8d86a55e5218f9d695d6e2e51b16
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2021-11-30 22:18:28 -0800

    styles for stat controls

--- src/components/Stat/Stats.jsx
+++ src/components/Stat/Stats.jsx
@@ -8,22 +8,25 @@ function Stats({
     handleClick
 }) {
     return (
-        <section className='stat-controls'>
         <>
+        <section className='stat-controls'>
             <h1>Add Catchphrase</h1>
+            <span className='catch-group'>
             <input
                 id='catchphrase'
                 name='catchphrase'
+                className='input'
                 aria-label='catchphrase-input'
                 data-testid='catchphrase-input' 
                 type='text' 
                 value={catchPhrase}
                 onChange={(e) => setCatchPhrase(e.target.value)}    
             />
+            </span>
             <label htmlFor='catchphrase' />
             <button type='button' onClick={handleClick}>Submit</button>
-            </>
         </section>
+            </>
     )
 }
 export default Stats
